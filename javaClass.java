8a8cc5aa974b21989a2287c60c80edb51b43e6e5
Rahat <rahatislamakash@gmail.com>
2022-04-11 16:16:28 +0600

    small program for check the java code

---
+++ javaClass.java
@@ -0,0 +1,9 @@
+public class javaClass {
+    public static void main(String[] args) {
+
+        System.err.println("hello world!");
+        System.err.println("please check the other repo for further info");
+        
+    }
+    
+}
